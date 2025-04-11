f0514e171306a138d9298283cd67da7ac8345e0b
YxngXi1 <xuetianyang@gmail.com>
2025-04-11 18:07:07 -0400

    fixed errors, hopefully doesn't break on deployment?

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 'use client'
 
 import { useState } from "react";
-import dynamic from "next/dynamic";
 import options from "@/utils/options";
+import dynamic from "next/dynamic";
 
-// Dynamically import the Wheel component with SSR disabled
-const Wheel = dynamic(() => import("react-custom-roulette").then((mod) => mod.Wheel), { ssr: false });
+// Dynamically load Wheel only on the client
+const Wheel = dynamic(
+  () => import("react-custom-roulette").then((mod) => mod.Wheel),
+  { ssr: false }
+);
 
 export default function Home() {
   const [mustSpin, setMustSpin] = useState(false);
@@ -17,19 +20,24 @@ export default function Home() {
     style: { backgroundColor: option.rigged ? "#FF5733" : "#33FF57" },
   }));
 
-  const handleSpinClick = () => {
+    const handleSpinClick = () => {
+    // rigging mechanism investment stuff
     const riggedOptions = localOptions.filter((option) => option.rigged);
-
+  
+    // probably won't be used but just a back up
     if (riggedOptions.length === 0) {
       alert("All choices have been selected");
       return;
     }
-
+  
+    // js choose a rnadom rigged option like as long as its in the list of riggedoptions it picks one of them
     const randomIndex = Math.floor(Math.random() * riggedOptions.length);
+  
+    // this is the actual finding and making the rigged option the rigged option
     const riggedPrizeIndex = localOptions.findIndex(
       (option) => option.name === riggedOptions[randomIndex].name
     );
-
+  
     setPrizeNumber(riggedPrizeIndex);
     setMustSpin(true);
   };
@@ -37,6 +45,7 @@ export default function Home() {
   const handleStopSpinning = () => {
     setMustSpin(false);
 
+    // Remove the guessed option
     setLocalOptions((prevOptions) =>
       prevOptions.filter((_, index) => index !== prizeNumber)
     );
@@ -44,8 +53,10 @@ export default function Home() {
 
   return (
     <>
+      {/* POPUP STUFF */}
       <div></div>
 
+      {/* MAIN PART */}
       <main>
         <h2>Wheel of Fortune</h2>
         <Wheel
